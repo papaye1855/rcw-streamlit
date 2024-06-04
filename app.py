9e78d56a3bf48db051d0acf802710742bbe1212c
teccarthelsinki <helsinki.461@gmail.com>
2024-06-04 17:06:43 -0400

    Connexion à snowflake et affichage des données

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 import streamlit as st
+import pandas as pd
+import snowflake.connector as sc
 
 def main():
-    st.write("### Bienvenue sur snowflake demo")
+    st.write("Demo de la connexion à snowflake depuis streamlit")
+
+    con = sc.connect(
+        account = 'xcsrvma-te06383',
+        user='papayecisse',
+        password = 'MonAccesSnowflake1@'
+    )
+
+    cursor = con.cursor()
+
+    def dataPersons():
+        sql = "Select * FROM rcw.persons.persons"
+        df  = cursor.execute(sql).fetchall()
+
+        return pd.DataFrame(df)
+    
+
+    donnees = dataPersons()
+    st.write(donnees)
+    
+
+    
+
+
+
 
 
 
